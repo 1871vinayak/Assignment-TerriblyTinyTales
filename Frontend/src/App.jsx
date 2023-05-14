39fd974aeb04cf8ebb7bb39eebb928bf3a8cdb5f
1871vinayak <1871vinayak@gmail.com>
2023-05-15 05:25:54 +0530

    Parsing The Content Parse the fetched content and find the frequency of occurrence of each word.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
+  const regex = /\b\w+\b/g;
+  const regex_expert = /[\w']*\p{L}*\w[\w.]*(?:-\w+.)?/gmu;
   const loadTextFile = async () => {
-    await axios.get('https://www.terriblytinytales.com/test.txt').then((data)=>{
-      console.log(data.data);
-    })
-    .catch((e)=>{
-      console.log(e);
-    })
-  }
+    await axios
+      .get("https://www.terriblytinytales.com/test.txt")
+      .then((data) => {
+        const array_string = data.data;
+        const filter_words = array_string.toLowerCase().match(regex);
+        const wordCounts = {};
+        filter_words.forEach((word) => {
+          wordCounts[word] = (wordCounts[word] || 0) + 1;
+        });
+        console.log(wordCounts);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
   return (
     <>
       <button onClick={loadTextFile}>Load Data</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
