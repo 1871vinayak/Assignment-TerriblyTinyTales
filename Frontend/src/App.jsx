2a9911efa172552ec56a0ec6aebd97b9f91f8995
1871vinayak <1871vinayak@gmail.com>
2023-05-15 05:58:40 +0530

    Filtering Of Data 1. Converting the Object into array of tuples, so that the word with its frequency count is stored in tuple. 2. Sorting the data

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,37 @@
 import axios from "axios";
 
 function App() {
+  //Regex to filter out the content
   const regex = /\b\w+\b/g;
   const regex_expert = /[\w']*\p{L}*\w[\w.]*(?:-\w+.)?/gmu;
+  const contentUrl = "https://www.terriblytinytales.com/test.txt";
+  //Loading of text file by fetching from URL
   const loadTextFile = async () => {
     await axios
-      .get("https://www.terriblytinytales.com/test.txt")
+      .get(contentUrl)
+      //If data if present
       .then((data) => {
-        const array_string = data.data;
-        const filter_words = array_string.toLowerCase().match(regex);
+        const arrayOf_string = data.data;
+        const filter_words = arrayOf_string.toLowerCase().match(regex);
         const wordCounts = {};
+        //Counting the total words
         filter_words.forEach((word) => {
           wordCounts[word] = (wordCounts[word] || 0) + 1;
         });
-        console.log(wordCounts);
+        //Creating tuple & Sorting 
+        const arrayOf_tuples = Object.entries(wordCounts);
+        const sortedTuples = arrayOf_tuples.sort((a, b) => b[1] - a[1]);
+        //Slicing top 20 words
+        const top20_data = sortedTuples.slice(0, 20);
+        console.log(top20_data);
       })
+      //Error if data not-present
       .catch((e) => {
         console.log(e);
       });
   };
   return (
+    //Button to load the data from the URL by calling the function
     <>
       <button onClick={loadTextFile}>Load Data</button>
     </>
